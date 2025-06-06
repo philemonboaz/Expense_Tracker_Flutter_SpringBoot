abc61fc10f3cdfe9c5abf4d89acf23883539316f
Boaz <philemonboaz@gmail.com>
2025-06-06 23:10:52 +0530

    Validation of Username and Password for JWT token generation.

--- src/main/java/com/example/expensetracker/interceptor/SecurityInterceptor.java
+++ src/main/java/com/example/expensetracker/interceptor/SecurityInterceptor.java
@@ -2,11 +2,22 @@ package com.example.expensetracker.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.Base64;
+
 @Component
+
 public class SecurityInterceptor implements HandlerInterceptor {
+
+    @Value("${auth.basic.username}")
+    private String BASIC_AUTH_USERNAME;
+
+    @Value("${auth.basic.password}")
+    private String BASIC_AUTH_PASSWORD;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
@@ -15,11 +26,28 @@ public class SecurityInterceptor implements HandlerInterceptor {
         // Custom logic: header check, token validation, logging, etc.
         String authHeader = request.getHeader("Authorization");
 
-        // Skip authorization check for /api/auth/** and /api/genToken/**
-        if (requestURI.startsWith("/api/auth/") || requestURI.startsWith("/api/genToken")) {
-            return true; // Skip authorization check
+        // Skip authorization check for /api/genToken/** - For Basic Auth
+        if (requestURI.startsWith("/api/genToken")) {
+            if (authHeader == null || !authHeader.startsWith("Basic ")) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Missing Basic Authorization header");
+                return false;
+            }
+            // Decode and validate
+            String base64Credentials = authHeader.substring("Basic ".length());
+            String credentials = new String(Base64.getDecoder().decode(base64Credentials));
+            String[] parts = credentials.split(":", 2);
+
+            if (parts.length != 2 || !parts[0].equals(BASIC_AUTH_USERNAME) || !parts[1].equals(BASIC_AUTH_PASSWORD)) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Invalid Basic Auth credentials");
+                return false;
+            }
+
+            return true;
         }
 
+        //Check for the Bearer Token
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write("Missing or invalid Authorization header");
